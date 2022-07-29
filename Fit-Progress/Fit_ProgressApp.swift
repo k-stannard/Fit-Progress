f1ea8cfc00cb71266854e4ad47de187d57a21d48
Koty Stannard <k.stannard@live.com>
2022-07-29 11:13:13 -0500

    Add xcode proj, notes file, and update .gitignore

---
+++ Fit-Progress/Fit_ProgressApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fit_ProgressApp.swift
+//  Fit-Progress
+//
+//  Created by Koty Stannard on 7/29/22.
+//
+
+import SwiftUI
+
+@main
+struct Fit_ProgressApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
